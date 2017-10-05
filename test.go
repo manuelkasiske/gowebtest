8d83a7f34e3dd346c22c92c11c08b99e4a89c526
Manuel Kasiske <manuel.kasiske@gmx.de>
2017-10-05 16:24:54 +0200

    webserver

---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gotschmarcel/goserv"
+	"net/http"
+		"log"
+)
+
+func main() {
+	server := goserv.NewServer()
+	server.Get("/", func (w http.ResponseWriter, r *http.Request) {
+		goserv.WriteString(w, "Welcome Home")
+	}
+	log.Fatalln(server.Listen(":12345"))
+}
